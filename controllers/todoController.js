1e758020007830b86e0b7746b462e2cbbc86108b
Louise Norris <louisenorris73@yahoo.co.uk>
2020-09-30 18:25:52 +0100

    added todo controller

---
+++ controllers/todoController.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+    app.get('/todo', function(res, res){
+
+    });
+
+    app.post('/todo', function(res, res){
+
+    });
+
+    app.delete('/todo', function(res, res){
+
+    });
+};
