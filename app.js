5b47a77091619127a56fab930042a88792c1309a
Louise Norris <louisenorris73@yahoo.co.uk>
2020-09-30 19:44:31 +0100

    added functionality to add and delete items on todo list

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
+// app.use(express.json()); 
+// app.use(express.urlencoded({extended: true}));
+
 //fire controllers
 todoController(app);
 
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,24 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'study'}, {item: 'eat crunch corner'}, {item: 'pose with pencil'}, {item: 'study more'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
-    app.get('/todo', function(res, res){
-        res.render('todo');
+    app.get('/todo', function(req, res){
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(res, res){
-
+    app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json(data);
+        console.log(data);
+        // debugger
     });
 
-    app.delete('/todo', function(res, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item
+        });
+        res.json(data)
     });
 };
